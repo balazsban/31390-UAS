02ff568bc840c300550d525c89fee4154641de3e
balazsban <balazs.ban.96@gmail.com>
2020-07-04 19:29:42 +0200

    Changes to linearized system

--- Part3/Ex3_1_constants.m
+++ Part3/Ex3_1_constants.m
@@ -8,7 +8,35 @@ Ixx = 3e-6
 Iyy = 3e-6
 Izz = 1e-5
 I = diag([Ixx, Iyy, Izz])
-g = [0, 0, -9.81]'
-mmat = diag([m, m, m])
+g = 9.81
+g_vec = [0, 0, -m*g]'
 attMtx = [L*k, 0, -L*k, 0; 0, L*k, 0, -L*k; b, -b, b, -b; 1, 1, 1, 1]
 posMtx = [0, -1, 0; 1, 0, 0]
+tmp = sqrt(m*g/4/k)
+omega_e = [tmp, tmp, tmp, tmp]'
+
+% Linearized system at hovering conditions
+% states: x, y, z, xdot, ydot, zdot, phi, theta, psi, wx, wy, wz
+AAA = [ 0 0 0 1 0 0 0 0 0 0 0 0;
+        0 0 0 0 1 0 0 0 0 0 0 0;
+        0 0 0 0 0 1 0 0 0 0 0 0;
+        0 0 0 -kD 0 0 0 m*9.81 0 0 0 0;
+        0 0 0 0 -kD 0 -9.81*m 0 0 0 0 0;
+        0 0 0 0 0 -kD 0 0 0 0 0 0;
+        0 0 0 0 0 0 0 0 0 1 0 0;
+        0 0 0 0 0 0 0 0 0 0 1 0;
+        0 0 0 0 0 0 0 0 0 0 0 1;
+        0 0 0 0 0 0 0 0 0 0 0 0;
+        0 0 0 0 0 0 0 0 0 0 0 0;
+        0 0 0 0 0 0 0 0 0 0 0 0];
+help = sqrt(9.81*m*k);
+help2 = sqrt(9.81*m/4/k);
+BBB = [0 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0; help help help help; 0 0 0 0; 0 0 0 0; 0 0 0 0;
+L/Ixx*help 0 -L/Ixx*help 0; 0 L/Iyy*help 0 -L/Iyy*help; 2*b/Izz*help2 -2*b/Izz*help2 2*b/Izz*help2 -2*b/Izz*help2];
+CCC = [ 1 0 0 0 0 0 0 0 0 0 0 0;
+        0 1 0 0 0 0 0 0 0 0 0 0;
+        0 0 1 0 0 0 0 0 0 0 0 0;
+        0 0 0 0 0 0 1 0 0 0 0 0;
+        0 0 0 0 0 0 0 1 0 0 0 0;
+        0 0 0 0 0 0 0 0 1 0 0 0];
+DDD = [0 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0];
